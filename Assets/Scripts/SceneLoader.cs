615d06961a3b28a9c69982570de9fc45baaaecbe
dapl3b <72482214+dapl3b@users.noreply.github.com>
2021-04-09 10:44:19 -0700

    Changed scene background and removed crossfade transition between scenes

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,33 +6,17 @@ using UnityEngine.UI;
 
 
 public class SceneLoader : MonoBehaviour
-{   
-    public Animator Transition;
-    public float TransitionTime = 1f;
-
-   public void LoadPlayerSelect()
-    {
-        StartCoroutine(LoadScene("PlayerSelectMenu"));
-    }
+{  
     public void LoadSettings()
     {
-        StartCoroutine(LoadScene("SettingsMenu"));
+        SceneManager.LoadScene("SettingsMenu");
     }
     public void LoadSingleplayer()
     {
-        StartCoroutine(LoadScene("SingleplayerQuiz"));
+        SceneManager.LoadScene("SingleplayerQuiz");
     }
     public void LoadHome()
     {
-        StartCoroutine(LoadScene("HomeMenu"));
-    }
-
-    IEnumerator LoadScene(string LevelName) 
-    {
-        Transition.SetTrigger("Start");
-
-        yield return new WaitForSeconds(TransitionTime);
-
-        SceneManager.LoadScene(LevelName);
+        SceneManager.LoadScene("HomeMenu");
     }
 }
